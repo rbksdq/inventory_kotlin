2003eb115ce76c7811651897248e59b845304c73
Rabeeka Sadiq <rabeeka.sadiq@10pearls.com>
2022-03-09 12:17:11 +0500

    just pushing this project

---
+++ src/main/kotlin/com/inventory_kotlin/inventory_kotlin/InventoryKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.inventory_kotlin.inventory_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InventoryKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<InventoryKotlinApplication>(*args)
+}
